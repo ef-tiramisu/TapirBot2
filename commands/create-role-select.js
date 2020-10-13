b4280755e027b444b80c4095df932165783a2cb1
Tira-Misuu <emily.firth98@gmail.com>
2020-10-13 17:32:30 +0200

    more embed

--- commands/create-role-select.js
+++ commands/create-role-select.js
@@ -4,11 +4,11 @@ module.exports = {
     execute(message) {
         const role_embed = new MessageEmbed()
       // Set the title of the field
-        .setTitle('Games')
+        .Title('Games')
       // Set the color of the embed
-        .setColor(0xff0000)
+        .Color(0xff0000)
       // Set the main content of the embed
-        .setDescription('Dota 2\nOverwatch\nAmong Us\nDnD\nMinecraft\nHearts of Iron');
+        .Description('Dota 2\nOverwatch\nAmong Us\nDnD\nMinecraft\nHearts of Iron');
     // Send the embed to the same channel as the message
         message.channel.send(role_embed);
     },
