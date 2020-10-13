62d0b8635ebede6320cbd7de58992b147a192807
Tira-Misuu <emily.firth98@gmail.com>
2020-10-13 17:55:23 +0200

    finally work?

--- commands/create-role-select.js
+++ commands/create-role-select.js
@@ -4,32 +4,13 @@ module.exports = {
     execute(message) {
         const exampleEmbed = {
             color: 0x0099ff,
-            title: 'Some title',
-            url: 'https://discord.js.org',
-            author: {
-                name: 'Some name',
-                icon_url: 'https://i.imgur.com/wSTFkRM.png',
-                url: 'https://discord.js.org',
-            },
-            description: 'Some description here',
+            title: 'Roles',
+
+            description: 'React for Roles',
             thumbnail: {
                 url: 'https://i.imgur.com/wSTFkRM.png',
             },
             fields: [
-                {
-                    name: 'Regular field title',
-                    value: 'Some value here',
-                },
-                {
-                    name: '\u200b',
-                    value: '\u200b',
-                    inline: false,
-                },
-                {
-                    name: 'Inline field title',
-                    value: 'Some value here',
-                    inline: true,
-                },
                 {
                     name: 'Inline field title',
                     value: 'Some value here',
@@ -41,16 +22,8 @@ module.exports = {
                     inline: true,
                 },
             ],
-            image: {
-                url: 'https://i.imgur.com/wSTFkRM.png',
-            },
             timestamp: new Date(),
-            footer: {
-                text: 'Some footer text here',
-                icon_url: 'https://i.imgur.com/wSTFkRM.png',
-            },
         };
-        
-        channel.send({ embed: exampleEmbed });
+        message.channel.send({ embed: exampleEmbed });
     },
 };
