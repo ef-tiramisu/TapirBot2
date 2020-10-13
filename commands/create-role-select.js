a28d2948b1ed303b30ad8ceacb86467c4cf30d06
Tira-Misuu <emily.firth98@gmail.com>
2020-10-13 17:48:52 +0200

    fix???

--- commands/create-role-select.js
+++ commands/create-role-select.js
@@ -1,10 +1,10 @@
 const { MessageEmbedImage } = require("discord.js");
-
+const Discord = require("discord.js");
 module.exports = {
-    name: 'create-select',
-    description: 'Lists all commands',
+    name: 'create-role-select',
+    description: 'create role select',
     execute(message) {
-        const Embed = new Discord.MessageEmbed().setTitle('Some title');        
+        const Embed = new Discord.MessageEmbed().setTitle('Some title');
         message.channel.send(Embed)
     },
 };
